66c31aaf00d4ad9ea9311cff9f07fd2ab3599a75
felipe.magagnin <felipebmagagnin@gmail.com>
2021-12-14 13:32:48 -0300

    hore: improva look and feel

---
+++ components/header.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+
+export default function Header({ wallet, isReady, address }) {
+    return (
+        <div className="header">
+            <a className="tittle colorGradient" href='/'>Fantom Chess</a>
+
+            <div className='menus'>
+                <div class="primary-navigation">
+                    <ul style={{padding: 0}}>
+                        <li>
+                            <a href="#">Fantom Chess</a>
+                            <ul class="dropdown">
+                                <li><a href="/">About/Mint</a></li>
+                                <li><a href="/view">My Games</a></li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="#">Fantom Chess Pieces</a>
+                            <ul class="dropdown">
+                                <li><a href="/">About (soon)</a></li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="#">🏆Tournaments🏆</a>
+                            <ul class="dropdown">
+                                <li><a href="/tournaments">Sign Up</a></li>
+                                <li><a href="/hall-of-fame">Hall of Fame</a></li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="/roadmap">Roadmap</a>
+                        </li>
+                    </ul>
+
+                </div>
+
+            </div>
+
+            <button className='button' onClick={wallet}>
+                {isReady ? address?.substring(0, 6) + "..." + address?.substring(address.length - 4, address.length) : "Connect"} { }
+            </button>
+        </div>
+    )
+}
--- pages/analyse/[id].js
+++ pages/analyse/[id].js
@@ -610,14 +610,11 @@ export default function analyse() {
                 </button>
             </div>
 
-            
-
             <div id='app'>
-
-            <div style={{backgroundColor: 'white', padding: 10, borderTopLeftRadius: 10, borderTopRightRadius: 10}}>
-                Fantom Chess #{id}
-            </div>
-                <div className="form-container" style={{ width: 'auto', height: 'auto', display: 'flex', justifyContent: 'center', marginBottom:50 }}>
+                <div style={{ backgroundColor: 'white', padding: 10, borderTopLeftRadius: 10, borderTopRightRadius: 10 }}>
+                    Fantom Chess #{id}
+                </div>
+                <div className="form-container" style={{ width: 'auto', height: 'auto', display: 'flex', justifyContent: 'center', marginBottom: 50 }}>
                     <div>
                         <div id='board' ></div>
                     </div>
