acba7cc49072b484dc9037cbce22f2fa99655822
felipe.magagnin <felipebmagagnin@gmail.com>
2022-03-02 10:29:47 -0300

    change roadmap order

--- pages/roadmap.js
+++ pages/roadmap.js
@@ -58,26 +58,6 @@ function Index () {
 
                     <div className='mobile-flex' style={{ display: 'flex', alignItems: 'flex-end' }}>
                         <div style={{ flex: 1, marginTop: 'auto', marginBottom: 'auto' }}>
-                            <h3 style={{ margin: 0 }} className='colorGradient'>$CHESS token</h3>
-                            <p>ERC20 Token, give back to the community a share of everything Fantom Chess earns :)</p>
-                        </div>
-
-                        <div style={{ flex: 1, textAlign: 'left', marginLeft: 30 }}>
-                            <p>🟦Create Token</p>
-                            <p>🟦Add liquidity (2º sales royalty fees)</p>
-                            <p>🟦Airdrop to Fantom Chess/Pieces holders</p>
-                            <p>🟦Implement new ways to increase token liquidity (30% of Fantom Chess 3D minting)</p>
-                            <p>🟦Create new ways to use the token</p>
-                        </div>
-                    </div>
-
-                    <hr></hr>
-
-                    <br></br>
-                    <br></br>
-
-                    <div className='mobile-flex' style={{ display: 'flex', alignItems: 'flex-end' }}>
-                        <div style={{ flex: 1, marginTop: 'auto', marginBottom: 'auto' }}>
                             <h3 style={{ margin: 0 }} className='colorGradient'>Fantom Chess 3D</h3>
                             <p>A NFT collection of random generated chess games in a 3D world, 30% of minting burned as $CHESS</p>
                         </div>
@@ -90,6 +70,26 @@ function Index () {
                             <p>🟦List on PaintSwap</p>
                             <p>🟦List on NFTKEY</p>
                             <p>🟦Sellout collection</p>
+                        </div>
+                    </div>
+
+                    <hr></hr>
+
+                    <br></br>
+                    <br></br>
+
+                    <div className='mobile-flex' style={{ display: 'flex', alignItems: 'flex-end' }}>
+                        <div style={{ flex: 1, marginTop: 'auto', marginBottom: 'auto' }}>
+                            <h3 style={{ margin: 0 }} className='colorGradient'>$CHESS token</h3>
+                            <p>ERC20 Token, give back to the community a share of everything Fantom Chess earns :)</p>
+                        </div>
+
+                        <div style={{ flex: 1, textAlign: 'left', marginLeft: 30 }}>
+                            <p>🟦Create Token</p>
+                            <p>🟦Add liquidity (2º sales royalty fees)</p>
+                            <p>🟦Airdrop to Fantom Chess/Pieces holders</p>
+                            <p>🟦Implement new ways to increase token liquidity (30% of Fantom Chess 3D minting)</p>
+                            <p>🟦Create new ways to use the token</p>
                         </div>
                     </div>
 
